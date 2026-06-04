8233cf0ab4e7a4301f6695476a5c415178f50ab3
emilianocamacho808-svg <emilianocamacho808@gmail.com>
2026-06-04 01:49:16 -0400

    creamos la clase base y un mixin.

--- logica.py
+++ logica.py
@@ -4,3 +4,15 @@ class CriaturaDebilitadaError(Exception):
     def __init__(self, mensaje="La criatura está debilitada y no puede continuar."):
         super().__init__(mensaje)
 
+class ElementoClimaticoMixin:
+    def aplicar_clima(self):
+        return "El clima afecta el campo de batalla."
+
+class EntidadBasica:
+    def __init__(self, nombre):
+        self._nombre = nombre
+        
+    @property
+    def nombre(self):
+        return self._nombre
+
